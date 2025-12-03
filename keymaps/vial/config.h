d73c73ac0400b5c8459ba9f5209600750f6a0f4d
Eromis <71084194+Eromis-keeb@users.noreply.github.com>
2025-12-03 21:04:33 +0900

    Add files via upload

---
+++ keymaps/vial/config.h
@@ -0,0 +1,28 @@
+// Copyright 2024 eromis (@kumasan555)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+
+#define VIAL_KEYBOARD_UID {0x59, 0x76, 0xD8, 0x45, 0xC4, 0xFA, 0x4C, 0x2E}
+#define VIAL_UNLOCK_COMBO_ROWS {0,0}
+#define VIAL_UNLOCK_COMBO_COLS {0,1}
+
+#define RGB_MATRIX_LED_COUNT 31
+
